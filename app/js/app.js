d6f97baa31f6cdfb49dd0711f5f600723db94007
joeberger <jberger@chariotsolutions.com>
2014-02-11 12:18:14 -0500

    Changed to use Angular-ui
    removed bootstrap and query as a crutches

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('restApp', [
   'ngRoute',
+  'ui.bootstrap',
   'restApp.filters',
   'restApp.services',
   'restApp.directives',
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,7 @@ angular.module('restApp.controllers', [])
   .controller('RequestCtrl', ['$scope', function($scope) {
   	//$scope.requestUrl = "my new url";
   	//$scope.sample = "hello";
+  	$scope.rVerb = "GET";
 	$scope.sendRequest = function() {
 		    	alert($scope.requestUrl + " " + $scope.rVerb);
 	};
